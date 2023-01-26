90ddd9f042b74ee6f004fb4bbd15653f85891c1d
marcelofgaraujo <marcelofgaraujo@gmail.com>
2023-01-26 11:02:46 -0300

    defining some dimensions of visual components

--- src/br/com/cod3r/calculator/view/Calculator.java
+++ src/br/com/cod3r/calculator/view/Calculator.java
@@ -1,5 +1,8 @@
 package br.com.cod3r.calculator.view;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class Calculator extends JFrame {
@@ -8,12 +11,25 @@ public class Calculator extends JFrame {
 	
 	public Calculator() {
 		
+		arrangeLayout();
+		
 		setSize(232, 322);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setVisible(true);
 	};
 	
+	private void arrangeLayout() {
+		setLayout(new BorderLayout());
+		
+		Display display = new Display();
+		display.setPreferredSize(new Dimension(233, 60));;
+		add(display, BorderLayout.NORTH);
+		
+		Keyboard keyboard = new Keyboard();
+		add(keyboard, BorderLayout.CENTER);
+	}
+
 	public static void main(String[] args) {
 		new Calculator();
 	}
